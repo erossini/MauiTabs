36292c4251c806bf4cf9f966785c22cc202efd56
Enrico Rossini <erossini@users.noreply.github.com>
2024-02-02 12:51:43 +0000

    Update MainPage.xaml.cs

--- MauiTabs/MainPage.xaml.cs
+++ MauiTabs/MainPage.xaml.cs
@@ -7,15 +7,5 @@
             InitializeComponent();
             BindingContext = new MainPageViewModel();
         }
-
-        //private void radio_CheckedChanged(object sender, CheckedChangedEventArgs e)
-        //{
-        //    if (((RadioButton)sender).IsChecked)
-        //    {
-        //        string option = ((RadioButton)sender).BindingContext.ToString();
-        //        if (labelSelection != null)
-        //            labelSelection.Text = option;
-        //    }
-        //}
     }
 }
